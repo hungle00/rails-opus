dbc924d8b0565edc7e20b21849b853c930c67b20
hungle00 <hungle2048@gmail.com>
2024-12-03 16:52:41 +0700

    Simply export photos data

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,19 @@
+require "csv"
+
 class Photo < ApplicationRecord
+  CSV_ATTRIBUTES = %w(id name url description likes_count).freeze
+
+  scope :visible, ->{ where.not(url: true) }
+
+  class << self
+  	# this method should be defined in service class
+    def export_data
+	  CSV.generate do |csv|
+	    csv << CSV_ATTRIBUTES
+	    Photo.all.each do |photo|
+	      csv << CSV_ATTRIBUTES.map{ |field| photo.send(field) }
+	    end
+	  end
+    end
+  end
 end
