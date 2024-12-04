9ca657696a2ddf5f6a38aef6977ead06cd037b76
hungle00 <hungle2048@gmail.com>
2024-12-04 17:33:50 +0700

    Export file with active-job and action-cable

--- app/controllers/photos/exports_controller.rb
+++ app/controllers/photos/exports_controller.rb
@@ -1,11 +1,16 @@
 class Photos::ExportsController < ApplicationController
   def index
-  	csv_data = Photo.export_data
-  	filename = "All_photos_#{DateTime.now.strftime("%Y%m%d_%H%M%S")}.csv"
+    ExportPhotosJob.perform_later
+    head :accepted
+  end
+
+  def download
+    job_id = params[:id]
+    filename = "All_photos_#{DateTime.now.strftime("%Y%m%d_%H%M%S")}.csv"
 
     respond_to do |format|
       format.csv do
-      	send_data csv_data, type: :csv, filename: filename
+        send_file Rails.root.join("tmp", "all_photos.csv"), type: :csv, filename: filename
       end
     end
   end
