2fa1ed1d4f457aeeaf97918bd65a84f92b22bba2
hungle00 <hungle2048@gmail.com>
2024-12-11 15:37:55 +0700

    Add faker gem, fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   sitepress_pages
   resources :projects
   resources :photos do
-    get :gallery, on: :collection    
+    get :gallery, on: :collection
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
